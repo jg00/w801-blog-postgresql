e054c46042cdfed8127c6f01f28c009fb99d5514
Jack Gamboa <jgamboa929@gmail.com>
2018-11-05 20:46:03 -0600

    CRUD

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.static('css'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+
 app.engine('mustache', mustacheExpress());
 app.set('views','./views');
 app.set('view engine', 'mustache');
@@ -17,16 +18,24 @@ const db = pgp(connectionString)
 
 
 
+// Will have login/registration
 app.get('/', (req,res) => {
     res.send('Login/Registration Page');
+    // After login in user should be redirected to thei page
+    // res.redirect('/blog/user');
 });
 
+
+// Display's all user posts
 app.get('/blog/user', (req,res) => {
+    // console.log('test get');
 
     let userid = 1;
     db.any('SELECT id, post, userid, entrydatetime FROM posts WHERE userid = $1', [userid])
-        .then((posts) => {
-            res.render('userblog', {posts: posts});
+        .then((results) => {
+            // console.log(results);
+
+            res.render('userblog', {posts: results});
         })
         .catch((err) => {
             console.log(err);
@@ -34,7 +43,54 @@ app.get('/blog/user', (req,res) => {
     });
 
 
+
+// Renders specific post page for update
+app.get('/blog/update/:id', (req,res) => {
+    let id = req.params.id;
+
+    // res.render('updatePost', {postEntry: id});
+
+    db.any('SELECT id, post, userid, entrydatetime FROM posts WHERE id = $1', [id])
+        .then((result) => {
+            // res.send(result);   // [{"id":22,"post":"test post 9","userid":1,"entrydatetime":"2018-11-06T00:08:20.365Z"}]
+            res.render('updatePost', {postEntry: result});
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+});
+
+
+
+
+// Update changes to specific post
+app.post('/blog/update/:id', (req,res) => {
+    // res.send("test");
+
+    let id = req.params.id;
+    let newPostUpdate = req.body.post;
+
+    db.one('UPDATE posts SET post = $1 WHERE id = $2 RETURNING (id)', [newPostUpdate, id])
+    .then((result) => {
+        console.log(result.id);
+        res.redirect('/blog/user');
+    })
+    .catch( (err) => {
+        console.log(err)
+    });  
+
+    // res.send(newPostUpdate);
+
+});
+
+
+
+
+
+// Handle new posts to be added and redirect
 app.post('/blog/user', (req,res) => {
+    // console.log('test post');
+    // console.log(req.body);          // { post: 'test post' }
 
     let post = req.body.post;
 
@@ -47,6 +103,39 @@ app.post('/blog/user', (req,res) => {
             console.log(err)
         });
 });
+
+
+// app.post('/blog/delete', (req,res) => {
+//     // console.log(req.body);   // { id: '16' }
+
+//     let id = req.body.id;
+//     db.none('DELETE FROM posts WHERE id = $1', [id])
+//         .then(() => {
+//             res.redirect('/blog/user')
+//         })
+//         .catch( (err) => {
+//             console.log(err)
+//         });
+// });
+
+
+// Delete user post
+app.post('/blog/delete/:id', (req,res) => {
+    // console.log(req.body);   // { id: '16' }
+
+    let id = req.params.id;
+    db.none('DELETE FROM posts WHERE id = $1', [id])
+        .then(() => {
+            res.redirect('/blog/user')
+        })
+        .catch( (err) => {
+            console.log(err)
+        });
+});
+
+
+
+
 
 
 
